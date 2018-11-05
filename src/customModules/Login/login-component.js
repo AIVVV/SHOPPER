0a3c8e0c9f8242c119869a139a9cf6c6292fd128
Angel Ivanov <a_ivanovvv@abv.bg>
2018-11-05 08:28:16 +0200

    Start to create login page - in progress;

--- src/customModules/Login/login-component.js
+++ src/customModules/Login/login-component.js
@@ -1,20 +1,31 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-
+const styles = theme => ({
+  root: {
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+});
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return(
-            <Fragment>
-                ala bala
-            </Fragment>
-        )
-    }
+  render() {
+
+      const { classes } = this.props;
+    return (
+      <Fragment>
+        <Paper square={true} className={classes.root}>In Paper</Paper>
+      </Fragment>
+    );
+  }
 }
 
-export default Login;
+export default withStyles(styles)(Login);
